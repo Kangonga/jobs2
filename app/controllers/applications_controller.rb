a628070075dc43e0b30ba8bb8374c7666d7ae085
Kangonga <guchu413@gmail.com>
2022-12-07 17:58:58 +0300

    complete deliverable

--- app/controllers/applications_controller.rb
+++ app/controllers/applications_controller.rb
@@ -13,4 +13,26 @@ class ApplicationsController < ApplicationController
     def show
         render json: Application.find(params[:id]), include:[:job, :user]
     end
+
+
+    def update
+        application = application_params
+        if application 
+         application.update!(user_id:params[:user_id], job_id:params[:job_id], application_stage:params[:application_stage])
+         render json: application
+        else
+         render json: {error:"application not found"}
+        end
+     end
+ 
+     def destroy
+         application = application_params
+         application.destroy
+         head :no_content
+     end
+ 
+     private
+    def application_params
+       Application.find_by(id: params[:id])
+    end
 end
