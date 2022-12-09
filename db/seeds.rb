89e6c4bae1f9d172371c48f68a933ad11cbc56fc
Leslie Ngwono Nyatigi <lesngwono84@gmail.com>
2022-12-09 07:37:22 +0300

    modified-seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,7 +9,6 @@ puts "seeding"
 10.times do
     User.create(username:Faker::Name.name, email: Faker::Internet.free_email, password: "12345")
 end
-# Job.create(job_title: Faker::Job.title,company_name: Faker::Company.name, description:Faker::Job.field, qualifications:Faker::Job.key_skill, deadline: Faker::Date.in_date_period(month: 12) , apply: true  )
 # Job.create(job_title: Faker::Job.title,company_name: Faker::Company.name, description:Faker::Job.field, qualifications:Faker::Job.key_skill, deadline: Faker::Date.in_date_period(month: 12) , apply: false )
 # Job.create(job_title: Faker::Job.title,company_name: Faker::Company.name, description:Faker::Job.field, qualifications:Faker::Job.key_skill, deadline: Faker::Date.in_date_period(month: 12) , apply: true )
 10.times do 
